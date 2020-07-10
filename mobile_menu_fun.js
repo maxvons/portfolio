30057c01189fa731c3b8e572c92b058f8762d337
maxvons <maximilianvons@gmail.com>
2020-07-10 20:26:39 +0200

    Experimental dark mode Small fixes to project card. Added hover and active link styling. Added dark mode toggle.

--- mobile_menu_fun.js
+++ mobile_menu_fun.js
@@ -1,13 +1,25 @@
 /* Toggle between adding and removing the "responsive" class to navbar 
     when the user clicks on the menu icon */
-    function toggleMobileMenu() {
-        var x = document.getElementById("navbar");
-        var m_div = document.getElementById("main-div");
-        if (x.className === "topnav") {
-            x.className += " responsive";
-            m_div.className += " top-extra-padding";
-        } else {
-            x.className = "topnav";
-            m_div.className = "top-padding";
-        }
+function toggleMobileMenu() {
+    var x = document.getElementById("navbar");
+    var m_div = document.getElementById("main-div");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+        m_div.className += " top-extra-padding";
+    } else {
+        x.className = "topnav";
+        m_div.className = "top-padding";
     }
+}
+
+function toggleDarkMode() {
+    var b = document.getElementsByTagName("body");
+    var logo = document.getElementById("logo");
+    if (b[0].className === "hide-scroll") {
+        b[0].className += " dark-mode";
+        logo.src = "icons/icon_white.svg";
+    }   else {
+        b[0].className = "hide-scroll";
+        logo.src="icons/portfolio_icon_fixed.svg";
+    }
+}
