4f302a86663f8e12d639a0cd2be232f5d4f9be6e
maxvons <maximilianvons@gmail.com>
2020-07-12 22:01:38 +0200

    Multiple changes Added animations to all pages. Added animations to contact form. Added underline animation to nav links. Added margin to com-pref-div. Added contact.php email script. Increased transition duration on link hover. Changed color on active input field on contact form. Added box shadow to hover state of submit button, and no shadow on active state. Changed font size of submit button on big screens.

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+    $my_email = "maximilianvons@gmail.com";
+    $name = htmlspecialchars($_POST['name']);
+    $sender_email  = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $pref = htmlspecialchars($_POST['com-pref']);
+    $msg = htmlspecialchars($_POST['message']);
+
+    echo  $name, ' ', $sender_email, ' ', $phone, ' ', $pref, ' ', $msg;
+    
+   
+    $to = $my_email;
+    $email_subject = "Contact form submission: $name";
+    $email_body = "You have received a message. Here are the details:\nName: $name\nEmail: $sender_email\nPhone: $phone\nContact preference: $pref\nMessage: $msg";
+    $headers = "From: maximilianvons@gmail.com" . "\r\n" . 
+                "Reply-To: $sender_email";
+    
+    mail($to,$email_subject,$email_body, $headers);
+
+    header('Location: contact_finished.html');
+?>
+
+
+
