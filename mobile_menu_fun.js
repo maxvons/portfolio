a2f65d0cad4e2e8dc0bb4f72c0ceb09d461389ce
maxvons <maximilianvons@gmail.com>
2020-07-16 20:41:17 +0200

    Dark Mode

--- mobile_menu_fun.js
+++ mobile_menu_fun.js
@@ -15,7 +15,44 @@ function toggleMobileMenu() {
     }
 }
 
-function toggleDarkMode() {
+const toggleColorMode = e => {
+
+    // Get logo 
+    var logo = document.getElementById("logo");
+
+    // Switch to light mode
+    if (e.currentTarget.classList.contains("light-hidden")) {
+
+        // Set custom html attribute
+        document.documentElement.setAttribute("color-mode", "light");
+        
+        // Set pref in local storage
+        localStorage.setItem('color-mode', 'light');
+
+        // Switch to black logo
+        logo.src = "icons/icon_black.svg";
+        return;
+    }
+
+    // Switch to dark mode
+    document.documentElement.setAttribute("color-mode", "dark");
+
+    // Set pref in local storage
+    localStorage.setItem("color-mode", "dark");
+
+    // Switch to white logo
+    logo.src = "icons/icon_white.svg";
+};
+    
+// Get the buttons in the DOM
+const toggleColorButtons = document.querySelectorAll(".color-mode_btn");
+
+// Set up event listeners
+toggleColorButtons.forEach(btn => {
+    btn.addEventListener("click", toggleColorMode);
+});
+    
+    /*
     var b = document.getElementsByTagName("body");
     var logo = document.getElementById("logo");
     if (b[0].className === "hide-scroll") {
@@ -25,4 +62,4 @@ function toggleDarkMode() {
         b[0].className = "hide-scroll";
         logo.src="icons/portfolio_icon_fixed.svg";
     }
-}
+    */
